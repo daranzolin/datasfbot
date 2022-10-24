8bc6797425a4e5fbb3409d104e126b9b1c8fcc3e
David Ranzolin <daranzolin@gmail.com>
2022-10-24 13:45:43 -0700

    Update datasfbot-tweet.R

--- datasfbot-tweet.R
+++ datasfbot-tweet.R
@@ -2,7 +2,7 @@
 datasfbot_token <- rtweet::rtweet_bot(
   api_key       = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
   api_secret    = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
-  access_token  = Sys.getenv("TWITTER_ACCESS_TOKEN")
+  access_token  = Sys.getenv("TWITTER_ACCESS_TOKEN"),
   access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 )
 
